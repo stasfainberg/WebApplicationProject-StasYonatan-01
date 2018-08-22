98df30242503ae56fe8a4517291074b69e4f7495
Stas Fainberg <stas.fainberg@gmail.com>
2018-08-22 12:36:43 +0300

    Initial Commit

---
+++ WebApplicationProject-StasYonatan-01/WebApplicationProject-StasYonatan-01/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace WebApplicationProject_StasYonatan_01.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
