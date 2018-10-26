bc6810ea6f80bee711fa98b4f73dbe085f2891dd
Stas Fainberg <stas.fainberg@gmail.com>
2018-10-26 14:32:56 +0300

    Stas-AddedNavbarDropdown-AddedModelsAttributes

--- Tachzukanit/TachzukanitBE/Models/Apartment.cs
+++ Tachzukanit/TachzukanitBE/Models/Apartment.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
@@ -19,6 +20,8 @@ namespace TachzukanitBE.Models
 
         public string Photo { get; set; }
 
+        [Range(1, 30)]
+        [DisplayName("Rooms Number")]
         public int RoomsNumber { get; set; }
 
         public ICollection<Malfunction> malfunctions { get; set; }
