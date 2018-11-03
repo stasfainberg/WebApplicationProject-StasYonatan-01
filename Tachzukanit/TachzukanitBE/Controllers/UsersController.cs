d0873716fc6ee66a293005dc2871bf4ca1f6aec1
danak <danak1995@gmail.com>
2018-11-03 19:19:08 +0200

    user index and details

--- Tachzukanit/TachzukanitBE/Controllers/UsersController.cs
+++ Tachzukanit/TachzukanitBE/Controllers/UsersController.cs
@@ -1,9 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using TachzukanitBE.Data;
+using Microsoft.AspNetCore.Authorization;
 
 namespace TachzukanitBE.Controllers
 {
@@ -16,9 +18,66 @@ namespace TachzukanitBE.Controllers
             _context = context;
         }
 
-        public IActionResult Index()
+        public async Task<IActionResult> Index(String searchString)
         {
-            return View();
+            if (String.IsNullOrEmpty(searchString))
+            {
+                return View(await _context.User.ToListAsync());
+            }
+
+            return View(await _context.User.Where(a => a.Address.Contains(searchString)).ToListAsync());
+        }
+
+        public async Task<IActionResult> Details(String id)
+        {
+            if (String.IsNullOrEmpty(id))
+            {
+                return NotFound();
+            }
+
+            var user = await _context.User
+                .FirstOrDefaultAsync(m => m.Id == id.ToString());
+
+            if (user == null)
+            {
+                return NotFound();
+            }
+
+            return View(user);
+        }
+
+        [Authorize(Roles = "Admin")]
+        public async Task<IActionResult> Edit(String id)
+        {
+            if (String.IsNullOrEmpty(id))
+            {
+                return NotFound();
+            }
+
+            var user = await _context.User.FindAsync(id);
+            if (user == null)
+            {
+                return NotFound();
+            }
+            return View(user);
+        }
+
+        [Authorize(Roles = "Admin")]
+        public async Task<IActionResult> Delete(String id)
+        {
+            if (String.IsNullOrEmpty(id))
+            {
+                return NotFound();
+            }
+
+            var user = await _context.User
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (user == null)
+            {
+                return NotFound();
+            }
+
+            return View(user);
         }
     }
 }
