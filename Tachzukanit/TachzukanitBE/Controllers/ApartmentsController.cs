bd0e79ca70cec3aabdc4e87fba8ad36623bcd278
Stas Fainberg <stas.fainberg@gmail.com>
2018-11-03 10:56:52 +0200

    User-Dropdown

--- Tachzukanit/TachzukanitBE/Controllers/ApartmentsController.cs
+++ Tachzukanit/TachzukanitBE/Controllers/ApartmentsController.cs
@@ -1,8 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Diagnostics;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
---
+++ Tachzukanit/TachzukanitBE/Controllers/UsersController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using TachzukanitBE.Data;
+
+namespace TachzukanitBE.Controllers
+{
+    public class UsersController : Controller
+    {
+        private readonly TachzukanitDbContext _context;
+
+        public UsersController(TachzukanitDbContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
