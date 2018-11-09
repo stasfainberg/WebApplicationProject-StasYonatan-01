ca5f277d0aa7e7b540ca09aa713363209f503d3f
Yonatan Ben Avraham <yonbav@gmail.com>
2018-11-09 07:49:14 +0200

    Tahzukanit BE =========== Adding a facebook post button to the layout

--- Tachzukanit/TachzukanitBE/wwwroot/js/site.js
+++ Tachzukanit/TachzukanitBE/wwwroot/js/site.js
@@ -1,4 +1,12 @@
 ﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
 // for details on configuring this project to bundle and minify static web assets.
 
+(function(d, s, id) {
+    var js, fjs = d.getElementsByTagName(s)[0];
+    if (d.getElementById(id)) return;
+    js = d.createElement(s); js.id = id;
+    js.src = "https://connect.facebook.net/en_US/sdk.js#xfbml=1&version=v3.0";
+    fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));
+
 // Write your JavaScript code.
