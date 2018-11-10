2fe6113cdda2ec1fb4ca802418fde5236e185635
danak <danak1995@gmail.com>
2018-11-10 19:08:11 +0200

    Naive bayes algorithm

--- Tachzukanit/TachzukanitBE/Controllers/StatisticsController.cs
+++ Tachzukanit/TachzukanitBE/Controllers/StatisticsController.cs
@@ -1,24 +1,33 @@
-﻿using System;
+﻿using System.Data;
 using System.Collections.Generic;
 using System.Globalization;
 using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Newtonsoft.Json;
 using TachzukanitBE.Data;
 using TachzukanitBE.Models;
+using ProbabilityFunctions;
+using System;
 
 namespace TachzukanitBE.Controllers
 {
     public class StatisticsController : Controller
     {
         private readonly TachzukanitDbContext _context;
+        private const int TRESHOLD = 5;
+        private DataTable table = new DataTable();
+        private DataSet dataSet = new DataSet();
+        
 
         public StatisticsController(TachzukanitDbContext context)
         {
             _context = context;
+            table.Columns.Add("Method");
+            table.Columns.Add("RoomNum", typeof(double));
+            table.Columns.Add("Month", typeof(double));
+            table.Columns.Add("Location", typeof(double));
         }
-        public ActionResult Index(string address)
+        public ActionResult Index(string address, string apartmentAddress, int month)
         {
             // ---- Bar Graph ----
             // Query with Join and Group By- using address parameter
@@ -51,7 +60,56 @@ namespace TachzukanitBE.Controllers
 
             ViewBag.Status = malfunctionsPieJson;
 
+            if (month != 0 && !String.IsNullOrEmpty(apartmentAddress))
+            {
+                Apartment apartment = _context.Apartment.FirstOrDefault(x => x.Address.Contains(apartmentAddress));
+                InitiallizeTrainData();
+                Classifier classifier = new Classifier();
+                classifier.TrainClassifier(table);
+                String name = classifier.Classify(new double[] { apartment.RoomsNumber, month, apartment.Longitude + apartment.Latitude });
+                ViewData["classification"] = name;
+            }
+
             return View();
+        }
+
+        private void InitiallizeTrainData()
+        {
+            var apartments = _context.Apartment.ToList();
+            foreach(Apartment apartment in apartments)
+            {
+                double location = apartment.Latitude + apartment.Longitude;
+                for (int i = 1; i <= 12; i++)
+                {
+                    if (apartment.malfunctions == null || apartment.malfunctions.Count < TRESHOLD)
+                    {
+                        table.Rows.Add("below", apartment.RoomsNumber, GetApartmentMalfByMonth(apartment, i), location);
+                    }
+                    else
+                    {
+                            table.Rows.Add("above", apartment.RoomsNumber, GetApartmentMalfByMonth(apartment, i), location);
+                    }
+                    
+                }
+            }
+            
+        }
+        private int GetApartmentMalfByMonth(Apartment apartment,int month)
+        {
+            int count = 0;
+
+            if (apartment.malfunctions != null)
+            {
+                foreach (Malfunction malf in apartment.malfunctions)
+                {
+                    if (malf.CreationDate.Month == month)
+                    {
+                        count++;
+                    }
+                }
+            }
+
+            return count;
         }
     }
 }
---
+++ Tachzukanit/TachzukanitBE/Data/Migrations/20181110103030_city.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace TachzukanitBE.Data.Migrations
+{
+    public partial class city : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "City",
+                table: "Apartment",
+                maxLength: 50,
+                nullable: false,
+                defaultValue: "");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "City",
+                table: "Apartment");
+        }
+    }
+}
